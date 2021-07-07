4d331b2f545cde862959855f653f65088f2cd5b6
Thiago Monteiro de Paula <thiago.paula@luizalabs.com>
2021-07-06 21:55:58 -0300

    feat: add ping route

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -3,6 +3,9 @@ import userRouter from '../routes/user/user.route';
 import {Router} from 'express';
 
 const router = Router();
+router.get('/ping', (req, res) => {
+  res.send('pong');
+});
 router.use('/user', userRouter);
 
 export default router;
